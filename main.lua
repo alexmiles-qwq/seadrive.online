ddb57bb5b0e5df8b71a89b83c65f0e07b3217c38
alexmiles-qwq <alexmiles33822@gmail.com>
2025-10-15 18:00:22 +0300

    pages fixes, new stuff
    New useHttps toggle, InsecureMode global variable sets to true if useHttps is false.

--- main.lua
+++ main.lua
@@ -7,6 +7,7 @@ Logger.minimalranktoprint = 2
 local __VERSION = '0.1.1'
 
 local https = require('https')
+local http = require('http')
 local timer = require('timer')
 
 local router = require('./router')
@@ -20,16 +21,20 @@ _G.AttachmentsDataStore = DataStore:GetDatastore('Attachments')
 _G.VisitsDataStore = DataStore:GetDatastore('VisitsDataStore')
 
 local options = {
-  key = fs.readFileSync(os.getenv("SSL_KEY_PATH")),
-  cert = fs.readFileSync(os.getenv("SSL_CERT_PATH"))
+  -- key = fs.readFileSync(os.getenv("SSL_KEY_PATH")),
+  -- cert = fs.readFileSync(os.getenv("SSL_CERT_PATH"))
   -- ca = fs.readFileSync('path/to/ca_bundle.pem'),
   -- requestCert = true,
   -- rejectUnauthorized = true
 }
 
+local useHttps = true
+if not useHttps then
+    _G.server.InsecureMode = true
+end
 
-local HttpsServer = https.createServer(options, function(req, res)
-
+local function OnReq(req, res)
+    
     local sockaddr = req.socket:address()
     local ip = sockaddr and sockaddr.ip or "unknown"
 
@@ -48,9 +53,19 @@ local HttpsServer = https.createServer(options, function(req, res)
         res:finish('Interal error')
     end
 
-end)
+end
 
-HttpsServer:listen(443)
+if useHttps then
+    local HttpsServer = https.createServer(options, OnReq)
+    HttpsServer:listen(443)
+else
+    Logger:Log('WARNING: Running server in insecure mode. Use it only for local tests!', 99)
+    local HttpServer = http.createServer(OnReq)
+    HttpServer:listen(80)
+end
+
+
+
 Logger:Log('Session Started at ' .. os.date('%x') .. '.\n Version: '..__VERSION..'.\n Made by AlexMiles.', 99)
 
 timer.setInterval(30 * 1000, function()
